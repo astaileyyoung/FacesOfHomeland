69ea6f8aab69896191314333497601fa64ba4e18
astaileyyoung <amosstaileyyoung@gmail.com>
2023-09-27 09:14:12 -0400

    ran clustering script

--- cluster_faces.py
+++ cluster_faces.py
@@ -2,15 +2,14 @@ import ast
 import shutil
 from pathlib import Path 
 
-import pandas as pd
-from tqdm import tqdm 
-
 import cv2
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from tqdm import tqdm 
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from tqdm import tqdm
+
 
 import sklearn.cluster as cluster
 from sklearn.decomposition import PCA
@@ -19,7 +18,7 @@ from sklearn.manifold import TSNE
 
 def get_images(files):
     data = []
-    for file in tqdm(files):
+    for file in files:
         temp, frame_num, face_num = file.stem.split('_')
         s = int(temp[1:3])
         e = int(temp[4:6])
@@ -32,6 +31,13 @@ def get_images(files):
         data.append(datum)
     fp_df = pd.DataFrame(data)
     return fp_df
+
+
+def get_clusters(data, algorithm, *args, **kwds):
+    labels = algorithm(*args, **kwds).fit_predict(data)
+    palette = sns.color_palette('deep', np.max(labels) + 1)
+    colors = [palette[x] if x >= 0 else (0,0,0) for x in labels]
+    return labels, colors
 
 
 def scatter_thumbnails(data, images, scale_factor=16, colors=None, show_images=True):
@@ -49,22 +55,24 @@ def scatter_thumbnails(data, images, scale_factor=16, colors=None, show_images=T
 
     if show_images:
         # add thumbnails :)
-        from matplotlib.offsetbox import OffsetImage, AnnotationBbox
-        for i in tqdm(range(len(images))):
+        for i in range(len(images)):
             image = plt.imread(images[i])
             h, w = image.shape[:2]
             n_h = int(h/scale_factor)
             n_w = int(w/scale_factor)
             image = cv2.resize(image, (n_w, n_h))
-            outputImage = cv2.copyMakeBorder(
-                 image, 
-                 10, 
-                 10, 
-                 10, 
-                 10, 
-                 cv2.BORDER_CONSTANT, 
-                 value=colors[i] if colors is not None else None)
-            im = OffsetImage(image)
+            if colors is not None:
+                outputImage = cv2.copyMakeBorder(
+                     image,
+                     2,
+                     2,
+                     2,
+                     2,
+                     cv2.BORDER_CONSTANT,
+                     value=colors[i] if colors is not None else None)
+                im = OffsetImage(outputImage)
+            else:
+                im = OffsetImage(image)
             bboxprops = dict(edgecolor=colors[i]) if colors is not None else None
             ab = AnnotationBbox(im, x[i], xycoords='data',
                                 frameon=(bboxprops is not None),
@@ -80,7 +88,7 @@ def save_images(df,
     if not dst.exists():
         Path.mkdir(dst, parents=True)
         
-    for idx, row in tqdm(episode_fp.iterrows(), total=episode_fp.shape[0]):
+    for idx, row in df.iterrows():
         fp = dst.joinpath(f'{row["label"]}/{Path(row["fp"]).name}')
         if not fp.parent.exists():
             Path.mkdir(fp.parent)
@@ -93,7 +101,7 @@ def save_fig(i,
     episode = f'S{str(i).zfill(2)}E{str(j).zfill(2)}'
     name = f'{episode}.png'
     fig_fp = fig_dir.joinpath(name)
-    plt.save_fig(str(fig_fp), dpi=300)
+    plt.savefig(str(fig_fp))
 
 
 def main():
@@ -104,6 +112,8 @@ def main():
                   how='left'
                  )
     files = [x for x in Path('./data/images').iterdir()]
+
+    print('Getting images ...')
     fp_df = get_images(files)
     df_fp = df.merge(fp_df,
                         on=['frame_num', 'face_num', 'season', 'episode'],
@@ -118,34 +128,37 @@ def main():
         Path.mkdir(clustered_dir)
 
     total = 8 * 12
-    pb = tqdm(total=total)
-    for i in range(8):
-        for j in range(12):
-            episode_fp = df_fp[(df_fp['season'] == i) & (df['episode'] == j)]
-
-            data = np.array([np.array(ast.literal_eval(x)) for x in episode_fp['encoding'].tolist()])
-            faces = episode_fp['fp'].tolist()
-        
-            x = PCA(n_components='mle', svd_solver='full').fit_transform(data)
-            tsne = TSNE(perplexity=50,
-                        n_components=2,
-                        learning_rate=50,
-                        n_iter=10000,
-                        early_exaggeration=300,
-                        # method='exact',
-                        n_iter_without_progress=300
-                     )
-            x = tsne.fit_transform(data)
-            ax = scatter_thumbnails(x, faces, scale_factor=16, show_images=True)
-            save_fig(i, j, fig_dir)
-            
-            labels, colors = get_clusters(x, cluster.DBSCAN, n_jobs=-1, eps=4.5, min_samples=15)
-            
-            ax = scatter_thumbnails(x, faces, scale_factor=16, show_images=True)
-            save_fig(i, j, clustered_dir)
-
+    pb = tqdm(total=total, desc='Clustering')
+    for i in range(1, 9):
+        for j in range(1, 13):
+            name = f'S{str(i).zfill(2)}E{str(j).zfill(2)}'
             dst = Path(f'./data/clustering/{name}')
-            save_images(episode_fp, dst)
+            if not dst.exists():
+                episode_fp = df_fp[(df_fp['season'] == i) & (df['episode'] == j)]
+
+                data = np.array([np.array(ast.literal_eval(x)) for x in episode_fp['encoding'].tolist()])
+                faces = episode_fp['fp'].tolist()
+
+                x = PCA(n_components='mle', svd_solver='full').fit_transform(data)
+                tsne = TSNE(perplexity=50,
+                            n_components=2,
+                            learning_rate=50,
+                            n_iter=10000,
+                            early_exaggeration=300,
+                            # method='exact',
+                            n_iter_without_progress=300
+                         )
+                x = tsne.fit_transform(x)
+                _ = scatter_thumbnails(x, faces, scale_factor=16, show_images=True)
+                save_fig(i, j, fig_dir)
+
+                labels, colors = get_clusters(x, cluster.DBSCAN, n_jobs=-1, eps=4, min_samples=15)
+                episode_fp['label'] = labels
+
+                _ = scatter_thumbnails(x, faces, colors=colors, scale_factor=16, show_images=True)
+                save_fig(i, j, clustered_dir)
+
+                save_images(episode_fp, dst)
             pb.update()
 
 
